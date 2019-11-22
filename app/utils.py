fd534d84648113ab53284f82f93f432644cbbb97
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-11-22 21:39:08 +0100

    added comments to utils methods, write output to file

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,6 @@
 import os
 import json
-
 import googleapiclient.discovery
-import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
@@ -13,17 +11,19 @@ def get_channel_id_from_url(url):
 
 
 def datetime_to_date(datetime):
+    # trim datetime string to date
     idx = datetime.find('T')
     date = datetime[:idx]
     return date
 
 
 def date_to_tuple(date):
-    date_tuple = tuple(date.split('-'))
+    date_tuple = tuple([int(n) for n in date.split('-')])
     return date_tuple
 
 
 def append_dates_from_results(response, list_of_dates):
+    # append api results to list of dates
     for item in response["items"]:
         date = datetime_to_date(item["snippet"]["publishedAt"])
         date_tuple = date_to_tuple(date)
@@ -31,7 +31,7 @@ def append_dates_from_results(response, list_of_dates):
 
 
 def get_list_of_dates(channel_id):
-
+    # get submisssion dates for 500 most recent videos from channel with channel_id
     with open('credentials.json', 'r') as file:
         credentials = json.load(file)
 
@@ -58,7 +58,7 @@ def get_list_of_dates(channel_id):
     nextPageToken = response["nextPageToken"]
 
     count = 0
-    while nextPageToken and count <= 9:
+    while nextPageToken and count < 2:
         request = youtube.search().list(
             part="snippet",
             relevanceLanguage="en",
@@ -73,17 +73,19 @@ def get_list_of_dates(channel_id):
         if response["nextPageToken"] is None:
             break
 
-    with open("results.json", "w") as file:
-        json.dump(response, file)
+    with open('tuples.txt', 'w') as file:
+        file.write(
+            '\n'.join(f"({t[0]}, {t[1]}, {t[2]})" for t in list_of_dates))
 
-    print(len(list_of_dates))
+    print(list_of_dates)
+
     return list_of_dates
 
 
 def main():
     cid = get_channel_id_from_url(
         "https://www.youtube.com/channel/UC-QDfvrRIDB6F0bIO4I4HkQ")
-    print(get_list_of_dates(cid))
+    dates = get_list_of_dates(cid)
 
 
 if __name__ == "__main__":
