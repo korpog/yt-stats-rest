fc2fd34b1abdbd55abec66aac5b5a73aa15301e8
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-11-25 22:26:06 +0100

    added view and url path

--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
+import utils
 
-def results(request):
-    pass
+
+@api_view
+def get_video_data(request):
+    cid = utils.get_channel_id_from_url(
+        "https://www.youtube.com/channel/UC-QDfvrRIDB6F0bIO4I4HkQ")
+    dates = utils.get_list_of_dates(cid)
+    results = utils.get_count_per_month(dates)
+    json_data = json.dumps(results)
+    return Response(json_data)
--- ytstats/urls.py
+++ ytstats/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views import get_video_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('results/', admin.site.urls),
+    path('results/', get_video_data, name='get-results'),
 ]
